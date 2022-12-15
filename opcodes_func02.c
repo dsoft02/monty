af3206769eb8770832273d874b2e99dc3dcec377
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-12-15 00:15:43 -0800

    implement the check comment function

--- opcodes_func02.c
+++ opcodes_func02.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+arg_t arg = {0, 0};
+
 /**
  * nop - does literally nothing
  * @stack: pointer to the head node pointer of stack
@@ -10,4 +12,27 @@ void nop(stack_t **stack, unsigned int nline)
 {
 	(void)stack;
 	(void)nline;
+}
+
+/**
+ * comment_check - checks if line is a comment
+ * @line: struct containing line contents and line number
+ *
+ * Return: true if comment, else false
+ */
+bool comment_check(line_t line)
+{
+	if (!line.content[0])
+	{
+		free(line.content);
+		return (true);
+	}
+
+	if (line.content[0][0] == '#')
+	{
+		free(line.content);
+		return (true);
+	}
+
+	return (false);
 }
