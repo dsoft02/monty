bc9ccdd203a1307ef66ed8816087860e87d2e32d
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-12-15 00:17:07 -0800

    implement the pchar, pstr opcode

--- opcodes_func02.c
+++ opcodes_func02.c
@@ -36,3 +36,68 @@ bool comment_check(line_t line)
 
 	return (false);
 }
+
+/**
+ * pchar - prints char at top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pchar(stack_t **stack, unsigned int nline)
+{
+	char c;
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	while (temp)
+	{
+		if (temp->prev == NULL)
+			break;
+		temp = temp->prev;
+	}
+
+	c = temp->n;
+	if (_isalpha(temp->n) == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", nline);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", c);
+}
+
+/**
+ * pstr - prints a str from ascii starting from the top
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pstr(stack_t **stack, unsigned int nline)
+{
+	int idx = 0;
+	char res[] = "";
+	char c;
+	stack_t *temp;
+
+	temp = *stack;
+	(void)nline;
+	/* starts at the top */
+	while (temp)
+	{
+		if (temp->n == 0)
+			break;
+		if (_isalpha(temp->n) == 0)
+			break;
+		c = temp->n;
+		printf("%c", c);
+		res[idx] += c;
+		temp = temp->next;
+		idx++;
+	}
+	printf("\n");
+}
