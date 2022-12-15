da52c0bab06dacb56815fdaabebd38aac84755cd
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-12-15 00:23:39 -0800

    implement rootl, rootr opcodes

--- opcodes_func02.c
+++ opcodes_func02.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-arg_t arg = {0, 0};
-
 /**
  * nop - does literally nothing
  * @stack: pointer to the head node pointer of stack
@@ -12,29 +10,6 @@ void nop(stack_t **stack, unsigned int nline)
 {
 	(void)stack;
 	(void)nline;
-}
-
-/**
- * comment_check - checks if line is a comment
- * @line: struct containing line contents and line number
- *
- * Return: true if comment, else false
- */
-bool comment_check(line_t line)
-{
-	if (!line.content[0])
-	{
-		free(line.content);
-		return (true);
-	}
-
-	if (line.content[0][0] == '#')
-	{
-		free(line.content);
-		return (true);
-	}
-
-	return (false);
 }
 
 /**
@@ -100,4 +75,38 @@ void pstr(stack_t **stack, unsigned int nline)
 		idx++;
 	}
 	printf("\n");
+}
+
+/**
+ * _isalpha - checks if int is in alphabet
+ * @c: int
+ * Return: 1 if yes, 0 if no
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * free_stack - frees all nodes in a stack
+ * @stack: pointer to the head node pointer of stack
+ *
+ * Return: Nothing.
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *temp = NULL;
+
+	if (stack == NULL || *stack == NULL)
+		return;
+
+	while (*stack != NULL)
+	{
+		temp = (*stack)->next;
+		free(*stack);
+		*stack = temp;
+	}
 }
