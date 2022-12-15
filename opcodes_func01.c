6ff8d19914fbdf5937811c0b69914ee937df842b
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-12-15 00:06:03 -0800

    Task 3 - implement the swap opcode

--- opcodes_func01.c
+++ opcodes_func01.c
@@ -106,3 +106,24 @@ void pop(stack_t **stack, unsigned int nline)
 		*stack = NULL;
 	}
 }
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void swap(stack_t **stack, unsigned int nline)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
